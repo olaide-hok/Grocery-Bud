1204165730360eb8326963215f5137795372ebf6
olaide-hok <olaidekareem43@yahoo.com>
2022-08-31 09:13:45 +0100

    Complete Alert Setup.
    Create showAlert fnc to handle alert state with setAlert callback.
    
    Pass showAlert as removeAlert prop to Alert component.
    
    Setup Alert component structure.
    
    Setup useEffect to remove alert after three seconds.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ function App() {
   const [list, setList] = useState([])
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+  const [alert, setAlert] = useState({ 
+    show: false,
+    msg: '',
+    type: ''
+  })
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (!name) {
       // display alert
+      showAlert(true, 'please enter value', 'danger',)
 
     } else if (name && isEditing) {
       // deal with editID
@@ -29,12 +34,16 @@ function App() {
 
   }
 
+  const showAlert = ( show = false, msg ='', type = '') => {
+    return setAlert({show, msg, type})
+  }
+
   return (
     <section className='section-center'>
       <form className="grocery-form"
         onSubmit={handleSubmit}
       >
-        {alert.show && <Alert/>}
+        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input type="text" className='grocery'
