942993694648a5a33ee20c3f920007988a3151a3
olaide-hok <olaidekareem43@yahoo.com>
2022-08-31 08:47:20 +0100

    Create Add Item Functionality.
    Setup handleSubmit to check for no item, when editing & adding new item.
    
    Conditionally show grocery lists when list length is greater than zero.
    
    Pass list items as prop to List component.
    
    Setup List component structure.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,22 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(name);
+    if (!name) {
+      // display alert
+
+    } else if (name && isEditing) {
+      // deal with editID
+
+    } else {
+      // show alert
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name
+      }
+      setList([...list, newItem])
+      setName('')
+    }
+
   }
 
   return (
@@ -32,10 +47,12 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
 
     </section>
   )
